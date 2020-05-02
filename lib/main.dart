3d539795d0b6dee82d76a67490f6a6eec757aaaf
Miguel López <miguelopezv@gmail.com>
2020-05-02 13:42:21 -0500

    add functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(
       home: BallPage(),
@@ -10,10 +11,21 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Image.asset('images/ball1.png')),
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
     );
   }
 }
